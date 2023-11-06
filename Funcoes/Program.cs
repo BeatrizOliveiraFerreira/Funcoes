852fbd20d5871ea44ca13db9d69293e7d1338d55
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-06 20:23:35 -0300

    Add project files.

---
+++ Funcoes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite três números");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            double resultado2 = Maior(3, 7, 4);
+
+            Console.WriteLine("Maior = " + resultado);
+        }
+
+        static int Maior(int a, int b, int c) {
+            int m;
+            if (a > b && a < c) {
+                m = a;
+            }
+            else if (b > c) {
+                m = b;
+            }
+            else {
+                m = c;
+            }
+            return m;
+        }
+    }
+}
